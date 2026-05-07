67b20217cf7cc45d67bf6070038f44fc3eb0db19
matheus-rdl <matheus_rdl@outlook.com>
2026-05-07 01:47:29 -0300

    Subindo alterações da sessão das bags

--- frontend/src/pages/bags/bags.jsx
+++ frontend/src/pages/bags/bags.jsx
@@ -4,6 +4,34 @@ export default function Bags() {
   return (
     <>
       <div className={styles.bags}>
+
+        <div className={styles.bags_content}>
+
+          <div className={styles.box}>
+            <img src="images/bags1.png" />
+            <div></div>
+            <p>Percussão</p>
+          </div>
+
+          <div className={styles.box}>
+            <img src="images/bags2.png" />
+            <div></div>
+            <p>Cordas</p>
+          </div>
+
+          <div className={styles.box}>
+            <img src="images/bags3.png" />
+            <div></div>
+            <p>Sopro</p>
+          </div>
+
+          <div className={styles.box}>
+            <img src="images/bags4.png" />
+            <div></div>
+            <p>Diversos</p>
+          </div>
+        </div>
+
       </div>
     </>
   );
--- frontend/src/pages/stage/stage.jsx
+++ frontend/src/pages/stage/stage.jsx
@@ -54,6 +54,32 @@ export default function Stage() {
           </div>
         </div>
       </div>
+
+      <div className={styles.stage_cards}>
+        <div className={`${styles.stage_cards_content_1}`}>
+          <img src="images/stage1.png" />
+          <div>
+            <p><span>Feito com paixão</span></p>
+            <p>Mais do que bags, entregamos segurança e confiança para músicos que levam sua arte a sério.</p>
+          </div>
+        </div>
+
+        <div className={`${styles.stage_cards_content_2}`}>
+          <img src="images/stage2.png" />
+          <div>
+            <p><span>Prazo que respeita sua música</span></p>
+            <p>Cada bag tem prazo definido e a gente cumpre. Organização e respeito com o seu tempo e sua arte.</p>
+          </div>
+        </div>
+
+        <div className={`${styles.stage_cards_content_3}`}>
+          <img src="images/stage3.png" />
+          <div>
+            <p><span>Segurança em cada entrega</span></p>
+            <p>O envio é feito com todo cuidado para que sua bag chegue perfeita até você.</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
