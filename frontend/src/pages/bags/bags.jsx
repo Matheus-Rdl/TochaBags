8634b1847a9cad098dffb90aa8d59a8f8b665396
Matheus <matheus_rdl@outlook.com>
2026-05-27 12:13:16 -0300

    main - Subindo alterações sobre a parte dos produtos das bags

--- frontend/src/pages/bags/bags.jsx
+++ frontend/src/pages/bags/bags.jsx
@@ -12,41 +12,33 @@ export default function Bags() {
           <div className={styles.box}>
             <img src="/images/bags1.png" />
             <div className={styles.box_icon}>
-              {/* 
               <img src="/images/b-icon1.png" />
-              */}
+              <p>Percussão</p>
             </div>
-            <p>Percussão</p>
           </div>
 
           <div className={styles.box}>
             <img src="/images/bags2.png" />
             <div className={styles.box_icon}>
-              {/* 
               <img src="/images/b-icon2.png" />
-              */}
+              <p>Cordas</p>
             </div>
-            <p>Cordas</p>
           </div>
 
           <div className={styles.box}>
             <img src="/images/bags3.png" />
             <div className={styles.box_icon}>
-              {/* 
               <img src="/images/b-icon3.png" />
-              */}
+              <p>Sopro</p>
             </div>
-            <p>Sopro</p>
           </div>
 
           <div className={styles.box}>
             <img src="/images/bags4.png" />
             <div className={styles.box_icon}>
-              {/* 
               <img src="/images/b-icon4.png" />
-              */}
+              <p>Diversos</p>
             </div>
-            <p>Diversos</p>
           </div>
         </div>
 
@@ -72,6 +64,47 @@ export default function Bags() {
             <div className={styles.box}>
               <img src="/images/bags4.png" />
               <p>Diversos</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.bags_products}>
+          <div className={styles.bags_list}>
+            <div className={styles.bags_card}>
+              <div>
+                <img src="/bags/cavacos/4bd06196-eee8-4226-b64b-0165da8b42ec/0.png" />
+              </div>
+              <h2>Bags - Cavacos</h2>
+            </div>
+            <div className={styles.bags_card}>
+              <div>
+                <img src="/bags/cavacos/4bd06196-eee8-4226-b64b-0165da8b42ec/1.png" />
+              </div>
+              <h2>Bags - Cavacos</h2>
+            </div>
+            <div className={styles.bags_card}>
+              <div>
+                <img src="/bags/cavacos/4bd06196-eee8-4226-b64b-0165da8b42ec/2.png" />
+              </div>
+              <h2>Bags - Cavacos</h2>
+            </div>
+            <div className={styles.bags_card}>
+              <div>
+                <img src="/bags/cavacos/4bd06196-eee8-4226-b64b-0165da8b42ec/3.png" />
+              </div>
+              <h2>Bags - Cavacos</h2>
+            </div>
+            <div className={styles.bags_card}>
+              <div>
+                <img src="/bags/cavacos/4bd06196-eee8-4226-b64b-0165da8b42ec/4.png" />
+              </div>
+              <h2>Bags - Cavacos</h2>
+            </div>
+            <div className={styles.bags_card}>
+              <div>
+                <img src="/bags/cavacos/4bd06196-eee8-4226-b64b-0165da8b42ec/5.png" />
+              </div>
+              <h2>Bags - Cavacos</h2>
             </div>
           </div>
         </div>
